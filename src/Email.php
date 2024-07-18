2fddc47806356c4c873092bef56285370988b282
anhh-nguyen-058 <anh.nguyen2@tda.company>
2024-07-18 17:35:36 +0700

    add rule validate number max length local domain path

--- src/Email.php
+++ src/Email.php
@@ -7,6 +7,7 @@ use Egulias\EmailValidator\Validation\RFCValidation;
 
 class Email
 {
+    public const MAX_LENGTH = 64;
     private EmailValidator $validator;
 
     public function __construct()
@@ -18,6 +19,10 @@ class Email
     {
         $domain = substr(strrchr($email, "@"), 1);
         if ($this->validator->isValid($email, new RFCValidation()) && strpos($domain, '.')) {
+            $arr = explode("@", $email);
+            if ($arr[0] > self::MAX_LENGTH || $arr[1] > self::MAX_LENGTH) {
+                return false;
+            }
             return true;
         }
         return false;
