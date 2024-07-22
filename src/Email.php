50952ebcf9c99fb517314917fe2057b81b51469c
anhh-nguyen-058 <anh.nguyen2@tda.company>
2024-07-22 13:33:55 +0700

    fix bug domain name contains more than 2 dots

--- src/Email.php
+++ src/Email.php
@@ -8,6 +8,7 @@ use Egulias\EmailValidator\Validation\RFCValidation;
 class Email
 {
     public const MAX_LENGTH = 64;
+    public const MAX_DOT_DOMAIN = 2;
     private EmailValidator $validator;
 
     public function __construct()
@@ -19,13 +20,22 @@ class Email
     {
         $domain = substr(strrchr($email, "@"), 1);
         if ($this->validator->isValid($email, new RFCValidation()) && strpos($domain, '.')) {
-            $arr = explode("@", $email);
-            if (strlen($arr[0]) > self::MAX_LENGTH || strlen($arr[1]) > self::MAX_LENGTH) {
-                return false;
-            }
-            return true;
+            return $this->isValidEmailRule((string)$email);
         }
         return false;
+    }
+
+    private function isValidEmailRule(string $email): bool
+    {
+        $arr = explode("@", $email);
+        $domain = $arr[1];
+        if (strlen($arr[0]) > self::MAX_LENGTH || strlen($domain) > self::MAX_LENGTH) {
+            return false;
+        }
+        if (substr_count($domain, ".") > self::MAX_DOT_DOMAIN) {
+            return false;
+        }
+        return true;
     }
 
 }
